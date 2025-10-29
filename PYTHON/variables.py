15e3dcc51bc50568654430a8ee10d4dcfd771b5a
Aditi Sonkar <imt_2023004@iiitm.ac.in>
2025-10-29 18:10:47 +0530

    updated

---
+++ PYTHON/variables.py
@@ -0,0 +1,22 @@
+# name="Aditi"
+# print("name")
+# print(name)
+
+# age=20
+# age2=age+1
+# print(age)
+# print(age2)
+
+# print("My name is:", name)
+# print("My age is:", age)
+
+a=5
+b=3
+sum=a+b
+diff=a-b
+print(sum)
+print(diff)
+print(a+b)
+print(sum+5)
+print(a+b+2)
+
